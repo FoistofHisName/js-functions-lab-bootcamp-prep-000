e6db03579360b162066436190fa525cd83a48689
Chris Sonsak <csonsak@gmail.com>
2018-07-12 21:31:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happHolidaysTo(holiday, name) {
 }
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} days until ${holiday}!`
+  return `It\'s ${days} days until ${holiday}!`
 }
